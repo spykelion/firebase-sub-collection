f5e05cbf84f5d3bccbb894493fc7df084ce8b5fd
NDI Lionel <59184392+spykelion@users.noreply.github.com>
2022-03-16 11:17:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const firebase = require("firebase");
 const db = require("./firebase-config");
 const app = express();
 
